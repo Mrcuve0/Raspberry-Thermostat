dbb2093444779c61e1a69abb7af35e0ae7326422
Paolo Calao <paolo.calao@gmail.com>
2019-05-19 17:26:24 +0200

    mqtt connection handled, db manager created

---
+++ Raspberry/db_manager.py
@@ -0,0 +1,29 @@
+from pymongo import MongoClient
+
+DB_ADDRESS = '192.168.43.154' #'localhost'
+
+client = MongoClient(DB_ADDRESS, 27017)
+logs_collection = client['logs']
+config_collection = client['config']
+
+def insert_log(log):
+	posts = logs_collection.posts
+	result = posts.insert(log)
+	return result
+
+def find_log(criteria):
+	posts = logs_collection.posts
+	result = posts.find_one(criteria)
+	return result
+
+
+
+
+#
+#post_data = {
+#	'title': 'Prova',
+#	'content': 'sto a prova'
+#}
+#result = posts.insert(post_data)
+#
+#print(posts.find_one({'title': 'Prova'}))
