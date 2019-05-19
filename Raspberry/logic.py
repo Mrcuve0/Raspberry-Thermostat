1dfac7c75ae07c623f586dd018e2bb13dad91f7a
Paolo Calao <paolo.calao@gmail.com>
2019-05-19 17:26:24 +0200

    mqtt connection handled, db manager created

--- Raspberry/logic.py
+++ Raspberry/logic.py
@@ -1,8 +1,13 @@
 import time
 import connection_manager
+import db_manager
 
-connection_manager.mqtt_connect()
+mqtt_manager = connection_manager.get_instance() 
+mqtt_manager.mqtt_connect()
 
 while True:
 	time.sleep(5)
-	print(connection_manager.get_last_message())
+	last_msg = mqtt_manager.get_last_temperatures()
+	print(last_msg)
+	#result = db_manager.insert_log({'msg': last_msg})
+	#print(result)
