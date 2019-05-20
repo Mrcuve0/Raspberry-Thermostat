1775ab59d3c3b33b0d1ae144ed16274e529174b1
Paolo Calao <paolo.calao@gmail.com>
2019-05-21 00:20:38 +0200

    db manager modified

--- Raspberry/db_manager.py
+++ Raspberry/db_manager.py
@@ -4,31 +4,39 @@ DB_ADDRESS = '192.168.43.154' #'localhost'
 
 client = MongoClient(DB_ADDRESS, 27017)
 logs_collection = client['logs']
-config_collection = client['config']
+config_collection = client['new_coll']
+test_collection = client['test']
 
 def insert_log(log):
 	posts = logs_collection.posts
 	result = posts.insert(log)
 	return result
 
-def find_log(criteria):
+def find_logs(criteria):
 	posts = logs_collection.posts
-	result = posts.find_one(criteria)
+	result = posts.find(criteria)
 	return result
 
 def get_configuration():
-	pass
+	posts = config_collection.posts
+	config = posts.find_one()
+	return config
 
-def get_last_temperatures():
-	pass
+def update_configuration(new_config):
+	config = get_configuration()
+	if config is not None:
+		new_config['_id'] = config['_id']
+	config_collection.posts.save(new_config)
 
+# Some tests
 
+update_configuration({'configa': 'prova'})
+print(get_configuration())
+update_configuration({'configa': 'sovrascritto'})
+print(get_configuration())
 
-#
-#post_data = {
-#	'title': 'Prova',
-#	'content': 'sto a prova'
-#}
-#result = posts.insert(post_data)
-#
-#print(posts.find_one({'title': 'Prova'}))
+print('    ')
+
+elements = config_collection.posts.find()
+for elem in elements:
+	print(elem)
