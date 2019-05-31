99419bc32ffd17d8297290e60c9b8a93046c505e
Mrcuve0 <cerini.samuele@outlook.it>
2019-05-31 15:53:28 +0200

    Add *settings window, password is obfuscated

--- Raspberry/GUI/addRoomSensorWindow.py
+++ Raspberry/GUI/addRoomSensorWindow.py
@@ -7,7 +7,6 @@
 # WARNING! All changes made in this file will be lost!
 
 from PyQt5 import QtCore, QtGui, QtWidgets
-# from mainWindow import Ui_MainWindow
 import mainWindow
 
 
@@ -15,19 +14,20 @@ class Ui_AddRoomSensorWindow(object):
 
     def on_PB_goBack_clicked(self):
         self.close()
-        self.window = QtWidgets.QMainWindow()
-        self.ui = mainWindow.Ui_MainWindow()
-        self.ui.setupUi(self.window)
-        self.window.show()
+        self.roomWindow = QtWidgets.QMainWindow()
+        self.uiMainWindow = mainWindow.Ui_MainWindow()
+        self.uiMainWindow.setupUi(self.roomWindow)
+        self.roomWindow.show()
 
     def activeFunctionsConnection(self):
         self.PB_goBack.clicked.connect(self.on_PB_goBack_clicked)
 
     def close(self):
-        self.window.close()
+        self.roomWindow.close()
 
     def setupUi(self, AddRoomSensorWindow):
-        self.window = AddRoomSensorWindow
+        self.roomWindow = AddRoomSensorWindow
+
         AddRoomSensorWindow.setObjectName("AddRoomSensorWindow")
         AddRoomSensorWindow.resize(800, 480)
         font = QtGui.QFont()
@@ -80,32 +80,51 @@ class Ui_AddRoomSensorWindow(object):
         font.setWeight(75)
         self.PB_goBack.setFont(font)
         self.PB_goBack.setObjectName("PB_goBack")
-        self.PT_room = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.PT_room.setGeometry(QtCore.QRect(110, 110, 581, 61))
+        self.LE_room = QtWidgets.QLineEdit(self.centralwidget)
+        self.LE_room.setGeometry(QtCore.QRect(110, 130, 581, 61))
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
         font.setWeight(75)
-        self.PT_room.setFont(font)
-        self.PT_room.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.PT_room.setSizeAdjustPolicy(
-            QtWidgets.QAbstractScrollArea.AdjustIgnored)
-        self.PT_room.setOverwriteMode(True)
-        self.PT_room.setObjectName("PT_room")
-        self.PT_sensor = QtWidgets.QPlainTextEdit(self.centralwidget)
-        self.PT_sensor.setGeometry(QtCore.QRect(110, 200, 581, 61))
+        self.LE_room.setFont(font)
+        self.LE_room.setObjectName("LE_room")
+        self.LE_sensor = QtWidgets.QLineEdit(self.centralwidget)
+        self.LE_sensor.setGeometry(QtCore.QRect(110, 240, 581, 61))
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
         font.setWeight(75)
-        self.PT_sensor.setFont(font)
-        self.PT_sensor.setVerticalScrollBarPolicy(QtCore.Qt.ScrollBarAlwaysOff)
-        self.PT_sensor.setSizeAdjustPolicy(
-            QtWidgets.QAbstractScrollArea.AdjustIgnored)
-        self.PT_sensor.setOverwriteMode(True)
-        self.PT_sensor.setObjectName("PT_sensor")
+        self.LE_sensor.setFont(font)
+        self.LE_sensor.setObjectName("LE_sensor")
+        self.label_RoomSettings = QtWidgets.QLabel(self.centralwidget)
+        self.label_RoomSettings.setGeometry(QtCore.QRect(-10, 0, 121, 61))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_RoomSettings.setFont(font)
+        self.label_RoomSettings.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_RoomSettings.setObjectName("label_RoomSettings")
+        self.label_RoomName = QtWidgets.QLabel(self.centralwidget)
+        self.label_RoomName.setGeometry(QtCore.QRect(90, 80, 161, 61))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_RoomName.setFont(font)
+        self.label_RoomName.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_RoomName.setObjectName("label_RoomName")
+        self.label_SensorID = QtWidgets.QLabel(self.centralwidget)
+        self.label_SensorID.setGeometry(QtCore.QRect(90, 190, 140, 61))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.label_SensorID.setFont(font)
+        self.label_SensorID.setAlignment(QtCore.Qt.AlignCenter)
+        self.label_SensorID.setObjectName("label_SensorID")
         self.PB_connect = QtWidgets.QPushButton(self.centralwidget)
-        self.PB_connect.setGeometry(QtCore.QRect(220, 290, 361, 61))
+        self.PB_connect.setGeometry(QtCore.QRect(220, 320, 361, 61))
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
@@ -131,10 +150,15 @@ class Ui_AddRoomSensorWindow(object):
             "AddRoomSensorWindow", "dd - MM - yyyy"))
         self.PB_ok.setText(_translate("AddRoomSensorWindow", "Ok"))
         self.PB_goBack.setText(_translate("AddRoomSensorWindow", "<"))
-        self.PT_room.setPlaceholderText(_translate(
+        self.LE_room.setPlaceholderText(_translate(
             "AddRoomSensorWindow", "Inserire nome della stanza"))
-        self.PT_sensor.setPlaceholderText(_translate(
+        self.LE_sensor.setPlaceholderText(_translate(
             "AddRoomSensorWindow", "Inserire identificativo del sensore"))
+        self.label_RoomSettings.setText(_translate("AddRoomSensorWindow", "Room\n"
+                                                   "Settings"))
+        self.label_RoomName.setText(_translate(
+            "AddRoomSensorWindow", "Room Name:"))
+        self.label_SensorID.setText(_translate(
+            "AddRoomSensorWindow", "Sensor ID:"))
         self.PB_connect.setText(_translate(
             "AddRoomSensorWindow", "Connetti..."))
-
