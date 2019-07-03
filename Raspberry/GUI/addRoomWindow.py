1965755c7613abacf3323644997ffc45859a03d2
Mrcuve0 <cerini.samuele@outlook.it>
2019-07-03 19:42:34 +0200

    Add new window: sensor, valve and program

--- Raspberry/GUI/addRoomWindow.py
+++ Raspberry/GUI/addRoomWindow.py
@@ -18,8 +18,9 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtCore import QTime, QDate, QTimer
 
 import settingsWindow
+
+# TODO: reinserire questa dipendenza
 # from Devices.connectionpy import connection
-from Devices.connectionpy import connection
 
 
 
