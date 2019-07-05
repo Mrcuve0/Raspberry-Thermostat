b1476ac3776d9bca06dc74efd5fc2e689b8a8e49
Mrcuve0 <cerini.samuele@outlook.it>
2019-07-05 11:54:35 +0200

    Add parentheses and minor formats

--- Devices/connectionpy/connection_sensor.py
+++ Devices/connectionpy/connection_sensor.py
@@ -1,42 +1,43 @@
 import bluetooth
 from bluetooth import *
 import time
-BTsocket=BluetoothSocket( RFCOMM )
+
+BTsocket = BluetoothSocket(RFCOMM)
 
 target_name = "ESP32test"
 target_address = None
 data = None
-#########################################################################
+
 def receiveMessages():
   global data
   data = BTsocket.recv(1)
-#########################################################################
+
 def connect(target_address):
   BTsocket.connect((target_address, 1))
-#########################################################################
+
 def disconnect():
   BTsocket.close()
-#########################################################################
+
 def sendIdentification():
   BTsocket.send(":termo")
   time.sleep(1)
-#########################################################################
+
 def sendssid():
   BTsocket.send("AndroidAP")
   time.sleep(1)
-#########################################################################
+
 def sendpsw():
   BTsocket.send("diosalame")
   time.sleep(1)
-#########################################################################
+
 def sendMQTT():
   BTsocket.send("mqttserver")
   time.sleep(1)
-#########################################################################
+
 def sendname():
   BTsocket.send("room_name")
   time.sleep(1)
-#########################################################################
+
 def lookUpNearbyBluetoothDevices():
   nearby_devices = bluetooth.discover_devices()
 
@@ -47,33 +48,33 @@ def lookUpNearbyBluetoothDevices():
           break
 
   if target_address is not None:
-      print "found target bluetooth device with address ", target_address
+      print("found target bluetooth device with address " + target_address)
   else:
-      print "could not find target bluetooth device nearby"
-#########################################################################
+      print("could not find target bluetooth device nearby")
+
 def connection():
   lookUpNearbyBluetoothDevices()
   connect(target_address)
   sendIdentification()
   receiveMessages()
-  print data
-  if data == '@':
+  print(data)
+  if (data == '@'):
     sendssid()
     receiveMessages()
-    print data
-    if data == '@':
+    print(data)
+    if (data == '@'):
       sendpsw()
       receiveMessages()
-      print data
-      if data == '@':
+      print(data)
+      if (data == '@'):
         sendMQTT()
         receiveMessages()
-        print data
-        if data == '@':
+        print(data)
+        if (data == '@'):
           sendname()
           receiveMessages()
-          print data
-          if data == '@':
+          print(data)
+          if (data == '@'):
             print ("connection successful")
             disconnect()
           else:
@@ -91,6 +92,6 @@ def connection():
   else:
     print ("connection unsuccessful")
     disconnect()
-#########################################################################
 
-connection()
+
+# connection()
