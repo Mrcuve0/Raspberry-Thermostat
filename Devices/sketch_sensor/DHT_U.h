05bf0c69b4d8f151c239261e1089dd40a4556d50
Mrcuve0 <cerini.samuele@outlook.it>
2019-07-07 19:16:21 +0200

    Fix libraries inclusions, fix temperature pointer in sketch <-- TOBECHECKED

--- Devices/sketch_sensor/DHT_U.h
+++ Devices/sketch_sensor/DHT_U.h
@@ -22,45 +22,48 @@
 #ifndef DHT_U_H
 #define DHT_U_H
 
-#include <Adafruit_Sensor.h>
-#include <DHT.h>
+#include "Adafruit_Sensor.h"
+#include "DHT.h"
 
 #define DHT_SENSOR_VERSION 1
 
-class DHT_Unified {
+class DHT_Unified
+{
 public:
-  DHT_Unified(uint8_t pin, uint8_t type, uint8_t count=6, int32_t tempSensorId=-1, int32_t humiditySensorId=-1);
+  DHT_Unified(uint8_t pin, uint8_t type, uint8_t count = 6, int32_t tempSensorId = -1, int32_t humiditySensorId = -1);
   void begin();
 
-  class Temperature : public Adafruit_Sensor {
+  class Temperature : public Adafruit_Sensor
+  {
   public:
-    Temperature(DHT_Unified* parent, int32_t id);
-    bool getEvent(sensors_event_t* event);
-    void getSensor(sensor_t* sensor);
+    Temperature(DHT_Unified *parent, int32_t id);
+    bool getEvent(sensors_event_t *event);
+    void getSensor(sensor_t *sensor);
 
   private:
-    DHT_Unified* _parent;
+    DHT_Unified *_parent;
     int32_t _id;
-
   };
 
-  class Humidity : public Adafruit_Sensor {
+  class Humidity : public Adafruit_Sensor
+  {
   public:
-    Humidity(DHT_Unified* parent, int32_t id);
-    bool getEvent(sensors_event_t* event);
-    void getSensor(sensor_t* sensor);
+    Humidity(DHT_Unified *parent, int32_t id);
+    bool getEvent(sensors_event_t *event);
+    void getSensor(sensor_t *sensor);
 
   private:
-    DHT_Unified* _parent;
+    DHT_Unified *_parent;
     int32_t _id;
-
   };
 
-  Temperature temperature() {
+  Temperature temperature()
+  {
     return _temp;
   }
 
-  Humidity humidity() {
+  Humidity humidity()
+  {
     return _humidity;
   }
 
@@ -70,9 +73,8 @@ private:
   Temperature _temp;
   Humidity _humidity;
 
-  void setName(sensor_t* sensor);
-  void setMinDelay(sensor_t* sensor);
-
+  void setName(sensor_t *sensor);
+  void setMinDelay(sensor_t *sensor);
 };
 
 #endif
--- Devices/sketch_sensor/sketch_sensor.ino
+++ Devices/sketch_sensor/sketch_sensor.ino
@@ -1,93 +1,103 @@
 #include "BluetoothSerial.h"
 #include <WiFi.h>
-#include <PubSubClient.h>
+#include "PubSubClient.h"
 #include <ESPmDNS.h>
 #include <WiFiClient.h>
 #include "Adafruit_Sensor.h"
 #include "DHT.h"
-/////////////////////////////////////////////////////////////////////
+
 #if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
 #error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
 #endif
-/////////////////////////////////////////////////////////////////////
-#define interruptPin 25     //Digital pin connected to the push button
-#define DHTPIN 27           // Digital pin connected to the DHT sensor
-#define DHTTYPE DHT22 
-/////////////////////////////////////////////////////////////////////
-const char* DEVICE_ID = "DEV00";
+
+#define interruptPin 25 //Digital pin connected to the push button
+#define DHTPIN 27       // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT22
+
+const char *DEVICE_ID = "DEV00";
 const int mqttPort = 1883;
-const char* mqttUser = "";
-const char* mqttPassword = "";
+const char *mqttUser = "";
+const char *mqttPassword = "";
 IPAddress mqttServer;
-/////////////////////////////////////////////////////////////////////
+
 char termo[] = ":termo";
 char test_transmission[20];
-/////////////////////////////////////////////////////////////////////
-float temperature;
-/////////////////////////////////////////////////////////////////////
-char ssidc[20] = "";
-char pswc[20] = "";
+
+float *temperature;
+
+char ssidc[64] = "";
+char pswc[64] = "";
 char mqttHostnamec[20] = "";
 char roomNamec[20] = "";
-/////////////////////////////////////////////////////////////////////
-char* ssid = ssidc;
-char* psw =  pswc;
-char* mqttHostname = mqttHostnamec;
-char* roomName = roomNamec;
-/////////////////////////////////////////////////////////////////////
+
+char *ssid = ssidc;
+char *psw = pswc;
+char *mqttHostname = mqttHostnamec;
+char *roomName = roomNamec;
+
 int pswc_index = 0;
 int ssidc_index = 0;
 int mqttHostnamec_index = 0;
 int test_index = 0;
 int wifi_timeout = 0;
 int roomNamec_index = 0;
-/////////////////////////////////////////////////////////////////////
-char ESPname[] = "ESP32test";
+
+// Device ID, change this for each ESP you are going to flash
+char ESPname[] = "Sensor 1";
 char ack_char = '@';
 char no_ack_char = '#';
-/////////////////////////////////////////////////////////////////////
+
 WiFiClient espClient;
 PubSubClient client(espClient);
 BluetoothSerial SerialBT;
 DHT dht(DHTPIN, DHTTYPE);
-/////////////////////////////////////////////////////////////////////
+
 /* Stupid mechanism to wait time without stopping the cpu */
 int start_time;
 const int time_interval = 5000;
-/////////////////////////////////////////////////////////////////////
-void callback(char* topic, byte* payload, unsigned int length) { 
+
+void callback(char *topic, byte *payload, unsigned int length)
+{
   Serial.print("Message arrived in topic: ");
   Serial.println(topic);
- 
+
   Serial.print("Message:");
-  for (int i = 0; i < length; i++) {
+  for (int i = 0; i < length; i++)
+  {
     Serial.print((char)payload[i]);
   }
- 
+
   Serial.println();
   Serial.println("-----------------------");
 }
-/////////////////////////////////////////////////////////////////////
-void setup() {
+
+void setup()
+{
   Serial.begin(115200);
   dht.begin();
   pinMode(interruptPin, INPUT_PULLUP);
   SerialBT.begin(ESPname); //Bluetooth device name
   Serial.println("The device started, now you can pair it with bluetooth!");
 
-////////////////////////////WAITING FOR LOW PIN//////////////////////
-  while(digitalRead(interruptPin) == HIGH){
-    if(SerialBT.available()){
-    Serial.println("wrong timing"); 
-    delay(1000);
-    SerialBT.end();
-    ESP.restart();
-    }  
+  ////////////////////////////WAITING FOR LOW PIN//////////////////////
+  while (digitalRead(interruptPin) == HIGH)
+  {
+    if (SerialBT.available())
+    {
+      Serial.println("wrong timing");
+      delay(1000);
+      SerialBT.end();
+      ESP.restart();
+    }
   }
   Serial.println("pin low, waiting for a transmission");
-////////////////////////////TEST TRANSMISSION////////////////////////
-  while(!SerialBT.available()){}
-  while(SerialBT.available()){
+
+  ////////////////////////////TEST TRANSMISSION////////////////////////
+  while (!SerialBT.available())
+  {
+  }
+  while (SerialBT.available())
+  {
     test_transmission[test_index] = SerialBT.read();
     Serial.println(test_transmission[test_index]);
     test_index++;
@@ -97,21 +107,28 @@ void setup() {
   test_transmission[strlen(test_transmission)] = '\0';
   Serial.println(test_transmission);
 
-  if(strcmp(test_transmission, termo) == 0){
+  if (strcmp(test_transmission, termo) == 0)
+  {
     Serial.println("test message recived correctly");
     SerialBT.write(ack_char);
     Serial.println("sent the ack character");
-  } else{
-    Serial.println("wrong host"); 
-    SerialBT.write(no_ack_char); 
+  }
+  else
+  {
+    Serial.println("wrong host");
+    SerialBT.write(no_ack_char);
     Serial.println("sent the no ack char");
     delay(1000);
     SerialBT.end();
     ESP.restart();
   }
-//////////////////////////////WIFI///////////////////////////////////
-  while(!SerialBT.available()){}
-  while(SerialBT.available()){
+
+  //////////////////////////////WIFI///////////////////////////////////
+  while (!SerialBT.available())
+  {
+  }
+  while (SerialBT.available())
+  {
     ssidc[ssidc_index] = SerialBT.read();
     Serial.println(ssidc[ssidc_index]);
     ssidc_index++;
@@ -122,8 +139,11 @@ void setup() {
   Serial.println(ssid);
   SerialBT.write(ack_char);
 
-  while(!SerialBT.available()){}
-  while(SerialBT.available()){
+  while (!SerialBT.available())
+  {
+  }
+  while (SerialBT.available())
+  {
     pswc[pswc_index] = SerialBT.read();
     Serial.println(pswc[pswc_index]);
     pswc_index++;
@@ -134,27 +154,33 @@ void setup() {
   Serial.println(pswc);
 
   WiFi.begin(ssid, psw);
- 
-  while (WiFi.status() != WL_CONNECTED) {
+
+  while (WiFi.status() != WL_CONNECTED)
+  {
     delay(500);
     wifi_timeout++;
-    if(wifi_timeout > 60){
-     Serial.println("wrong credentials");  
-     SerialBT.write(no_ack_char); 
-     Serial.println("sent the no ack char");
-     delay(1000); 
-     SerialBT.end();
-     ESP.restart();
+    if (wifi_timeout > 60)
+    {
+      Serial.println("wrong credentials");
+      SerialBT.write(no_ack_char);
+      Serial.println("sent the no ack char");
+      delay(1000);
+      SerialBT.end();
+      ESP.restart();
     }
     Serial.println("Connecting to WiFi..");
   }
 
   Serial.println("Connected to the WiFi network");
   SerialBT.write(ack_char);
-  Serial.println("sent the ack char"); 
-//////////////////////////////MQTT///////////////////////////////////
-  while(!SerialBT.available()){}
-  while(SerialBT.available()){
+  Serial.println("sent the ack char");
+
+  //////////////////////////////MQTT///////////////////////////////////
+  while (!SerialBT.available())
+  {
+  }
+  while (SerialBT.available())
+  {
     mqttHostnamec[mqttHostnamec_index] = SerialBT.read();
     Serial.println(mqttHostnamec[mqttHostnamec_index]);
     mqttHostnamec_index++;
@@ -164,49 +190,54 @@ void setup() {
   mqttHostnamec[strlen(mqttHostnamec)] = '\0';
   Serial.println(mqttHostnamec);
 
-
-  if (!MDNS.begin(DEVICE_ID)) {
+  if (!MDNS.begin(DEVICE_ID))
+  {
     Serial.println("Error setting up MDNS responder!");
     delay(1000);
-    SerialBT.write(no_ack_char); 
+    SerialBT.write(no_ack_char);
     Serial.println("sent the no ack char");
-    delay(1000); 
+    delay(1000);
     SerialBT.end();
     ESP.restart();
   }
-  
-   
+
   Serial.println("mDNS responder started");
-  // Look for the local IP of the rasbperry pi 
+  // Look for the local IP of the rasbperry pi
   mqttServer = MDNS.queryHost(mqttHostname);
   Serial.print("IP address of server: ");
   Serial.println(mqttServer.toString());
-  // Connect to the MQTT broker 
+  // Connect to the MQTT broker
   client.setServer(mqttServer, mqttPort);
   client.setCallback(callback);
-    
-  while (!client.connected()) {
+
+  while (!client.connected())
+  {
     Serial.println("Connecting to MQTT...");
-      
+
     //if (client.connect("ESP32Client", mqttUser, mqttPassword )) {
-    if (client.connect(ESPname, mqttUser, mqttPassword )) {
+    if (client.connect(ESPname, mqttUser, mqttPassword))
+    {
       Serial.println("Connected to the broker");
       SerialBT.write(ack_char);
       Serial.println("sent the ack char");
-    } else {
-      Serial.println("wrong credentials for the broker");  
-      SerialBT.write(no_ack_char); 
+    }
+    else
+    {
+      Serial.println("wrong credentials for the broker");
+      SerialBT.write(no_ack_char);
       Serial.println("sent the no ack char");
-      delay(1000); 
+      delay(1000);
       SerialBT.end();
       ESP.restart();
     }
-    
   }
 
-//////////////////////////////ROOM///////////////////////////////////
-  while(!SerialBT.available()){}
-  while(SerialBT.available()){
+  //////////////////////////////ROOM///////////////////////////////////
+  while (!SerialBT.available())
+  {
+  }
+  while (SerialBT.available())
+  {
     roomNamec[roomNamec_index] = SerialBT.read();
     Serial.println(roomNamec[roomNamec_index]);
     roomNamec_index++;
@@ -220,19 +251,19 @@ void setup() {
   Serial.println("setup done, everything is connected");
   SerialBT.end();
   Serial.println("SerialBT ended");
-  
+
   start_time = millis();
   Serial.println("initialized the start time");
-
 }
 
-/////////////////////////////////////////////////////////////////////
-void loop() {
-  if (millis() - start_time > time_interval){
-    temperature = dht.readTemperature();
+void loop()
+{
+  if (millis() - start_time > time_interval)
+  {
+    *temperature = dht.readTemperature();
     Serial.print("temperature ");
-    Serial.print(t);
-    client.publish("temperature/ESPname", temperature);       //ESPname has to be changed with a number (the ID)
+    Serial.print(*temperature);
+    client.publish("temperature/ESPname", (char *)&temperature, 5); //ESPname has to be changed with a number (the ID)
     start_time = millis();
   }
 }
