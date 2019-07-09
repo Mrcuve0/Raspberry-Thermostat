57f53a36d98c94b0a9b8922399f1f2009ea03336
Mrcuve0 <cerini.samuele@outlook.it>
2019-07-09 09:41:55 +0200

    Add hostname thermostat

--- Devices/connectionpy/connection_sensor.py
+++ Devices/connectionpy/connection_sensor.py
@@ -61,7 +61,7 @@ def sendpsw(BTsocket, net_PWD):
   time.sleep(1)
 
 def sendMQTT(BTsocket):
-  BTsocket.send("mqttserver")
+  BTsocket.send("thermostat")
   time.sleep(1)
 
 def sendname(BTsocket, roomName):
