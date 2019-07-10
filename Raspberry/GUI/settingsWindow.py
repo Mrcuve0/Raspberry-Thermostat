906d1099b422b36242ad6b1c99ef645c12ffa9fb
Mrcuve0 <cerini.samuele@outlook.it>
2019-07-10 19:24:28 +0200

    Add delete buttons

--- Raspberry/GUI/settingsWindow.py
+++ Raspberry/GUI/settingsWindow.py
@@ -4,7 +4,9 @@ from PyQt5.QtCore import QTime, QDate, QTimer
 import mainWindow
 import networkSettingsWindow
 import addRoomWindow
+import deleteRoomWindow
 import addActuatorWindow
+import deleteActuatorWindow
 
 
 class Ui_SettingsWindow(object):
@@ -35,6 +37,13 @@ class Ui_SettingsWindow(object):
         self.uiAddRoomWindow.setupUi(self.settingsWindow, self.db)
         self.settingsWindow.showMaximized()
 
+    def on_PB_DeleteRoom_clicked(self):
+        self.close()
+        self.settingsWindow = QtWidgets.QMainWindow()
+        self.uiDeleteRoomWindow = deleteRoomWindow.Ui_deleteRoomWindow()
+        self.uiDeleteRoomWindow.setupUi(self.settingsWindow, self.db)
+        self.settingsWindow.showMaximized()
+
     def on_PB_AddActuator_clicked(self):
         self.close()
         self.settingsWindow = QtWidgets.QMainWindow()
@@ -42,12 +51,20 @@ class Ui_SettingsWindow(object):
         self.uiAddActuatorWindow.setupUi(self.settingsWindow, self.db)
         self.settingsWindow.showMaximized()
 
+    def on_PB_DeleteActuator_clicked(self):
+        self.close()
+        self.settingsWindow = QtWidgets.QMainWindow()
+        self.uiDeleteActuatorWindow = deleteActuatorWindow.Ui_deleteActuatorWindow()
+        self.uiDeleteActuatorWindow.setupUi(self.settingsWindow, self.db)
+        self.settingsWindow.showMaximized()
 
     def activeFunctionsConnection(self):
         self.PB_goBack.clicked.connect(self.on_PB_goBack_clicked)
         self.PB_network.clicked.connect(self.on_PB_network_clicked)
         self.PB_AddRoom.clicked.connect(self.on_PB_AddRoom_clicked)
+        self.PB_DeleteRoom.clicked.connect(self.on_PB_DeleteRoom_clicked)
         self.PB_AddActuator.clicked.connect(self.on_PB_AddActuator_clicked)
+        self.PB_DeleteActuator.clicked.connect(self.on_PB_DeleteActuator_clicked)
         self.timer.timeout.connect(self.showTime)
         self.showTime()
         self.timer.start(1000)
@@ -70,28 +87,8 @@ class Ui_SettingsWindow(object):
         SettingsWindow.resize(800, 480)
         self.centralwidget = QtWidgets.QWidget(SettingsWindow)
         self.centralwidget.setObjectName("centralwidget")
-        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
-        self.dateEdit.setGeometry(QtCore.QRect(120, 0, 571, 61))
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
-        font.setKerning(True)
-
-        # Date and Time widgets
-        self.dateEdit.setFont(font)
-        self.dateEdit.setInputMethodHints(QtCore.Qt.ImhDate)
-        self.dateEdit.setWrapping(False)
-        self.dateEdit.setFrame(False)
-        self.dateEdit.setAlignment(QtCore.Qt.AlignCenter)
-        self.dateEdit.setReadOnly(True)
-        self.dateEdit.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
-        self.dateEdit.setObjectName("dateEdit")
         self.timeEdit = QtWidgets.QTimeEdit(self.centralwidget)
         self.timeEdit.setGeometry(QtCore.QRect(687, 0, 121, 61))
-
-        self.timer = QTimer()
-
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
@@ -104,7 +101,21 @@ class Ui_SettingsWindow(object):
         self.timeEdit.setReadOnly(True)
         self.timeEdit.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
         self.timeEdit.setObjectName("timeEdit")
-
+        self.dateEdit = QtWidgets.QDateEdit(self.centralwidget)
+        self.dateEdit.setGeometry(QtCore.QRect(120, 0, 571, 61))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        font.setKerning(True)
+        self.dateEdit.setFont(font)
+        self.dateEdit.setInputMethodHints(QtCore.Qt.ImhDate)
+        self.dateEdit.setWrapping(False)
+        self.dateEdit.setFrame(False)
+        self.dateEdit.setAlignment(QtCore.Qt.AlignCenter)
+        self.dateEdit.setReadOnly(True)
+        self.dateEdit.setButtonSymbols(QtWidgets.QAbstractSpinBox.NoButtons)
+        self.dateEdit.setObjectName("dateEdit")
         self.PB_goBack = QtWidgets.QPushButton(self.centralwidget)
         self.PB_goBack.setGeometry(QtCore.QRect(0, 380, 111, 100))
         font = QtGui.QFont()
@@ -113,10 +124,6 @@ class Ui_SettingsWindow(object):
         font.setWeight(75)
         self.PB_goBack.setFont(font)
         self.PB_goBack.setObjectName("PB_goBack")
-        font = QtGui.QFont()
-        font.setPointSize(16)
-        font.setBold(True)
-        font.setWeight(75)
         self.PB_network = QtWidgets.QPushButton(self.centralwidget)
         self.PB_network.setGeometry(QtCore.QRect(180, 80, 581, 61))
         font = QtGui.QFont()
@@ -126,7 +133,7 @@ class Ui_SettingsWindow(object):
         self.PB_network.setFont(font)
         self.PB_network.setObjectName("PB_network")
         self.PB_AddRoom = QtWidgets.QPushButton(self.centralwidget)
-        self.PB_AddRoom.setGeometry(QtCore.QRect(180, 160, 581, 61))
+        self.PB_AddRoom.setGeometry(QtCore.QRect(180, 160, 281, 61))
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
@@ -134,14 +141,32 @@ class Ui_SettingsWindow(object):
         self.PB_AddRoom.setFont(font)
         self.PB_AddRoom.setObjectName("PB_AddRoom")
         self.PB_AddActuator = QtWidgets.QPushButton(self.centralwidget)
-        self.PB_AddActuator.setGeometry(QtCore.QRect(180, 240, 581, 61))
+        self.PB_AddActuator.setGeometry(QtCore.QRect(180, 240, 281, 61))
         font = QtGui.QFont()
         font.setPointSize(16)
         font.setBold(True)
         font.setWeight(75)
         self.PB_AddActuator.setFont(font)
         self.PB_AddActuator.setObjectName("PB_AddActuator")
+        self.PB_DeleteRoom = QtWidgets.QPushButton(self.centralwidget)
+        self.PB_DeleteRoom.setGeometry(QtCore.QRect(480, 160, 281, 61))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.PB_DeleteRoom.setFont(font)
+        self.PB_DeleteRoom.setObjectName("PB_DeleteRoom")
+        self.PB_DeleteActuator = QtWidgets.QPushButton(self.centralwidget)
+        self.PB_DeleteActuator.setGeometry(QtCore.QRect(480, 240, 281, 61))
+        font = QtGui.QFont()
+        font.setPointSize(16)
+        font.setBold(True)
+        font.setWeight(75)
+        self.PB_DeleteActuator.setFont(font)
+        self.PB_DeleteActuator.setObjectName("PB_DeleteActuator")
         SettingsWindow.setCentralWidget(self.centralwidget)
+
+        self.timer = QTimer()
 
         self.initDB(db)
 
@@ -151,12 +176,12 @@ class Ui_SettingsWindow(object):
 
     def retranslateUi(self, SettingsWindow):
         _translate = QtCore.QCoreApplication.translate
-        SettingsWindow.setWindowTitle(
-            _translate("SettingsWindow", "MainWindow"))
+        SettingsWindow.setWindowTitle(_translate("SettingsWindow", "MainWindow"))
         self.timeEdit.setDisplayFormat(_translate("SettingsWindow", "HH : mm"))
-        self.dateEdit.setDisplayFormat(
-            _translate("SettingsWindow", "dd - MM - yyyy"))
+        self.dateEdit.setDisplayFormat(_translate("SettingsWindow", "dd - MM - yyyy"))
         self.PB_goBack.setText(_translate("SettingsWindow", "<"))
         self.PB_network.setText(_translate("SettingsWindow", "Network"))
         self.PB_AddRoom.setText(_translate("SettingsWindow", "Add Room"))
         self.PB_AddActuator.setText(_translate("SettingsWindow", "Add Actuator"))
+        self.PB_DeleteRoom.setText(_translate("SettingsWindow", "Delete Room"))
+        self.PB_DeleteActuator.setText(_translate("SettingsWindow", "Delete Actuator"))
