fdd26ed99b046c0522c8a3bc167db18c8a685253
Mrcuve0 <cerini.samuele@outlook.it>
2019-07-15 04:16:54 +0200

    Commit before night test

--- Raspberry/constants.py
+++ Raspberry/constants.py
@@ -7,9 +7,9 @@ antifreeze_settings = 'antifreeze'
 programmable_settings = 'programmable'
 # Actuator constants
 actuator_hot = 'hot'
-actuator_hot_topic = 'actuator/hot'
+actuator_hot_topic = 'actuator/hot/'
 actuator_cold = 'cold'
-actuator_cold_topic = 'actuator/cold'
+actuator_cold_topic = 'actuator/cold/'
 # Power constants
-power_on = 'on'
-power_off = 'off'
+power_on = 'ON'
+power_off = 'OFF'
